315c3fbb92306fd6ce0647eb55a7170900dc1914
PluwJin <ozerhan9@gmail.com>
2020-02-19 22:56:27 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Header from './components/Header';
 import Detail from './pages/detail';
 import Create from './pages/create';
 
--- src/components/helloworld.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-const helloworld = () =>{ return (<h1>HelloWorld</h1>) }
-
-export default helloworld;
